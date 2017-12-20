98884877861c3455cc054ce87f2950df8b881b90
takuter <34451313+takuter@users.noreply.github.com>
2017-12-20 10:02:14 -0500

    First Working Commit, mess around with other variations

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -2,6 +2,11 @@ package quiz;
 
 public class ReverseString {
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+    	StringBuffer reversal = new StringBuffer();
+    	for(int i= input.length() - 1; i>=0 ; i--)
+    		reversal.append(input.charAt(i));
+    	
+    	return reversal.toString();
+        //return new StringBuffer(input).reverse().toString();
     }
 }
